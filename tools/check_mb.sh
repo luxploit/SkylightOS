f474d39d3fb10877ba8f323d1959ad0e9f97a93c
pinksub <subpurple_alt@outlook.com>
2023-06-07 21:18:15 -0400

    Make GRUB check simpler

--- tools/check_mb.sh
+++ tools/check_mb.sh
@@ -5,40 +5,20 @@ if [ ! -f ./SConstruct ]; then
   exit 1
 fi
 
-echo "Checking grub version..."
-dowehavegrub2=0
-echo "Attempting to use grub2- prefix..."
-output=$(grub2-install --version 2>/dev/null)
-if [[ $output =~ "2." ]]; then
-   echo "grub2 found"
-   dowehavegrub2=1
+if [ -x "$(command -v grub2-install)" ]; then
+   version=$(grub2-install --version)
    prefix="grub2-"
-fi
-# i have no idea why but grub 1.9x is also grub2
-if [[ $output =~ "1.9" ]]; then
-   echo "grub2 found"
-   dowehavegrub2=1
-   prefix="grub2-"
-fi
-if [ -z "$output" ]; then
-echo "Attempting to use grub- prefix..."
-output=$(grub-install --version 2>/dev/null)
-if [[ $output =~ "2." ]]; then
-   echo "grub2 found"
-   dowehavegrub2=1
+elif [ -x "$(command -v grub-install)" ]; then
+   version=$(grub-install --version)
    prefix="grub-"
-fi
-# i have no idea why but grub 1.9x is also grub2
-if [[ $output =~ "1.9" ]]; then
-   echo "grub2 found"
-   dowehavegrub2=1
-   prefix="grub-"
-fi
+else
+   echo "ERR: unable to find GRUB prefix"
+   exit 1
 fi
 
-if [ "$dowehavegrub2" -eq "0" ]; then
-   echo "ERR: unable to find grub2";
-   exit 1;
+if ! ( [[ $version =~ "2." || $version =~ "1.9" ]] ) then
+   echo "GRUB2 is required"
+   exit 1
 fi
 
 set -e
