3132513539a7dd058210fb63d644ef7d5aaa6559
Seal Sealy <butilkama@gmail.com>
2023-06-16 07:17:52 +0300

    Kernel: Detect FPU, just making this for the future of the project

---
+++ src/crt/libk/math/fpu.c
@@ -0,0 +1,13 @@
+/* fpu.c
+ * Created on Fri Jun 16 2023 by Seal Sealy (seal331)
+ * Codename "Esaul" Operating System
+ * Copyright (c) 2023 - SkylightOS Project
+*/
+
+char* _esaul_nofpu() {
+    return "0";
+}
+
+char* _esaul_hasfpu() {
+    return "1";
+}
---
+++ src/crt/sdk/kernel/fpu.h
@@ -0,0 +1,9 @@
+/* fpu.h
+ * Created on Fri Jun 16 2023 by Seal Sealy (seal331)
+ * Codename "Esaul" Operating System
+ * Copyright (c) 2023 - SkylightOS Project
+*/
+
+extern char* detectfpu();
+char* _esaul_nofpu();
+char* _esaul_hasfpu();
