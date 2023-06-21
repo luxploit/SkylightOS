6ac45f5f112f447ca95bab70f159bdf113f1f77c
Laura Raine <lu.raine@outlook.jp>
2023-06-21 03:30:11 +0200

    stuff i need to stash for now

---
+++ tools/build/new_osver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# new_osver.py
+# Created on Tue Jun 20 2023 by Laura Raine (lnkexploit)
+# Added for Codename: "Esaul"
+# Copyright (c) 2023 - SkylightOS Project
+
+import config as esos_config
